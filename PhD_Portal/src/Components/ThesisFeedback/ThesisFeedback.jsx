5b2bb58818e6c2d3244255b5aab5848ce17fc4c9
Craig Rosario <craig.rosario04@gmail.com>
2024-12-07 13:41:19 +0530

    Added basic edits

--- PhD_Portal/src/Components/ThesisFeedback/ThesisFeedback.jsx
+++ PhD_Portal/src/Components/ThesisFeedback/ThesisFeedback.jsx
@@ -48,7 +48,7 @@ const ThesisFeedback = () => {
                         <div className='mainEval'>Lorem ipsum dolor sit amet.</div>
 
                         <label for="EvalReport" className='labelEvalReport'>Evaluation Report</label>
-                        <div className='evalReport'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, a!</div>
+                        <div className='evalReport'>The evaluation of the thesis has been completed, and the following feedback is provided. The thesis demonstrates a strong understanding of the subject and uses effective methodologies. However, improvements are recommended in the presentation of the data, particularly in the results section. Some sections require better integration of recent studies to enhance the literature review. Additionally, while the conclusion is clear, it could benefit from more actionable recommendations.</div>
                     </div>
                 </div>
             )}
@@ -64,7 +64,7 @@ const ThesisFeedback = () => {
                         <div className='mainEval'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
 
                         <label htmlFor="EvalReport" className='labelEvalReport'>Evaluation Report</label>
-                        <div className='evalReport'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus fugiat, maxime impedit aperiam recusandae iste praesentium eveniet voluptatibus veniam ex. Laudantium soluta alias deserunt autem!</div>
+                        <div className='evalReport'>The thesis is well-researched, with a clear and coherent structure. The methodology used is appropriate for the research question, and the data analysis is sound. However, some areas could be improved for clarity, particularly in the explanation of the results. There is a need for more detailed justification of the research choices, particularly with regard to the sample size and data collection methods.</div>
                     </div>
                 </div>
             )}
