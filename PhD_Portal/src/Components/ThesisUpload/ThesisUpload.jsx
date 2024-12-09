ccab8dd783f2f62ef2dbe7219128827b12f816d6
Craig Rosario <craig.rosario04@gmail.com>
2024-12-09 13:11:33 +0530

    Made thesis upload page with file uploads and submit button

--- PhD_Portal/src/Components/ThesisUpload/ThesisUpload.jsx
+++ PhD_Portal/src/Components/ThesisUpload/ThesisUpload.jsx
@@ -7,7 +7,7 @@ const ThesisUpload = () => {
   return (
     <>
       <div className='navBar'>
-        <div className='hero'>Thesis Upload</div>
+        <div className='hero'>Thesis Submission</div>
         <div className="buttons">
           <button className='notiBtn'>
             <img src={noti} alt="Notification" />
@@ -16,6 +16,27 @@ const ThesisUpload = () => {
             <img src={prof} alt="Profile" />
           </button>
         </div>
+      </div>
+
+
+      <div className='upload'>
+        <div className='uploadThesis'>
+          <p className='Title'>Upload Thesis</p>
+          <div className='mainUpload'>
+            <input type="file" />
+          </div>
+        </div>
+
+        <div className='uploadSupportingDocs'>
+          <p className='Title'>Upload Supporting Documents</p>
+          <div className='mainUpload'>
+            <input type="file" />
+          </div>
+        </div>
+      </div>
+
+      <div className='submit'>
+        <button className='submitBtn'>Submit</button>
       </div>
     </>
   )
